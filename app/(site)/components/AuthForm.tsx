cc23552c95acb92a0478b4904b2ae717c0eaea7c
Ahemd Bappy <bappy3a@gmail.com>
2023-05-27 21:57:42 +0600

    login desine done

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,5 +1,6 @@
  'use client';
 
+import Button from "@/app/components/Button";
 import Input from "@/app/components/inputs/input";
 import { useCallback, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
@@ -14,7 +15,7 @@ const AuthForm = () => {
         if(variant === 'LOGIN'){
             setVariant('REGISTER')
         } else{
-            setVariant('REGISTER')
+            setVariant('LOGIN')
         }
     },[variant])
 
@@ -48,8 +49,33 @@ const AuthForm = () => {
         <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
             <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
                 <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
-                    <Input id="email" label="Email" register={register} errors={errors} />
+                    { variant === 'REGISTER' && (
+                        <Input id="name" label="Name" register={register} errors={errors} />
+                    )}
+                    <Input id="email" label="Email Address" type="email" register={register} errors={errors} />
+                    <Input id="pasword" label="Password" type="password" register={register} errors={errors} />
+                    <div >
+                        <Button disabled={IsLoading} fullWidth type="submit" >{ variant === 'LOGIN' ? 'Sign In' : 'Register' }</Button>
+                    </div>
                 </form>
+                <div className="md-6">
+                    <div className="relative">
+                        <div className="absolute inset-0 flex items-center">
+                            <div className="w-full border-t border-gray-300" />
+                        </div>
+                        <div className="relative flex justify-center text-sm">
+                            <span className="bg-white px-2 text-gray-500 mt-2">
+                                Or continue with
+                            </span>
+                        </div>
+                    </div>
+                    <div className="flex gap-2 justify-center text-sm mt-6 px-2 text-gray-500">
+                        <div>{variant === 'LOGIN' ? 'New to Messenger?' : 'Already have an account?'}  </div>
+                        <div onClick={toggleVariant} className="underline cursor-pointer">
+                            {variant === 'LOGIN' ? 'Create an account' : 'Login'}
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
      );
